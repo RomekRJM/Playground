753547016db81f32546100d585b81754faefc330
Roman Subik <roman.subik@ocado.com>
2017-11-01 18:31:32 +0100

    Simple fight simulation

--- LearningPathBeginningCpp/brute.cpp
+++ LearningPathBeginningCpp/brute.cpp
@@ -3,100 +3,131 @@
 #include <ctime>
 using namespace std;
 
-class Weapon{
+class Weapon {
 private:
     string name;
     float minDamage;
     float maxDamage;
 public:
+
+    Weapon() : Weapon("Bare hands", 1.0, 3.0) {
+    };
     Weapon(string _name, float _minDamage, float _maxDamage);
     float hit();
 };
 
-class DamageInterceptor{
+class DamageInterceptor {
 public:
     virtual float dealDamage(float * hp, Weapon weapon);
 };
 
-class CriticalDamageInterceptor : public DamageInterceptor{
+class CriticalDamageInterceptor : public DamageInterceptor {
 private:
     const float criticalMultiplier = 1.5;
 public:
-        float dealDamage(float * hp, Weapon weapon) override;
+    float dealDamage(float * hp, Weapon weapon) override;
 };
 
 template <class T>
-class Character{
+class Character {
 private:
     string name;
     T hitPoints;
     DamageInterceptor* damageInterceptor;
+    Weapon weapon;
 public:
     Character(string name, T hitPoints);
     string getName();
     T getHitPoints();
-    float dealDamage(Weapon weapon);
+    float receiveDamage(Character aggressor);
     void setDamageInterceptor(DamageInterceptor* damageInterceptor);
+    void setWeapon(Weapon w);
+    Weapon getWeapon();
 };
 
 template <class T>
-Character<T>::Character(string nname, T initialHitPoints){
+Character<T>::Character(string nname, T initialHitPoints) {
     name = nname;
     hitPoints = initialHitPoints;
     damageInterceptor = new DamageInterceptor();
-    }
+}
 
 template <class T>
-string Character<T>::getName(){
+string Character<T>::getName() {
     return name;
 }
 
 template <class T>
-T Character<T>::getHitPoints(){
+T Character<T>::getHitPoints() {
     return hitPoints;
 }
 
 template <class T>
-float Character<T>::dealDamage(Weapon weapon){
-    return damageInterceptor->dealDamage(&hitPoints, weapon);
+void Character<T>::setWeapon(Weapon w) {
+    weapon = w;
 }
 
 template <class T>
-void Character<T>::setDamageInterceptor(DamageInterceptor* _damageInterceptor){
-        damageInterceptor = _damageInterceptor;
+Weapon Character<T>::getWeapon() {
+    return weapon;
 }
 
-float DamageInterceptor::dealDamage(float* hp, Weapon weapon){
-    cout << "DamageInterceptor" << endl;
-        return *hp -= weapon.hit();
+template <class T>
+float Character<T>::receiveDamage(Character aggressor) {
+    return damageInterceptor->dealDamage(&hitPoints, aggressor.getWeapon());
 }
 
-float CriticalDamageInterceptor::dealDamage(float* hp, Weapon weapon){
-    cout << "CriticalDamageInterceptor" << endl;
-    return *hp -= weapon.hit() * criticalMultiplier;
+template <class T>
+void Character<T>::setDamageInterceptor(DamageInterceptor* _damageInterceptor) {
+    damageInterceptor = _damageInterceptor;
 }
 
-Weapon::Weapon(string _name, float _minDamage, float _maxDamage){
+float DamageInterceptor::dealDamage(float* hp, Weapon weapon) {
+    float hit = weapon.hit();
+    *hp -= hit;
+    return hit;
+}
+
+float CriticalDamageInterceptor::dealDamage(float* hp, Weapon weapon) {
+    float hit = weapon.hit() * criticalMultiplier;
+    *hp -= hit;
+    return hit;
+}
+
+Weapon::Weapon(string _name, float _minDamage, float _maxDamage) {
     name = _name;
     minDamage = _minDamage;
     maxDamage = _maxDamage;
 }
 
-float Weapon::hit(){
-    srand (static_cast <unsigned> (time(0)));
-    return minDamage + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(maxDamage-minDamage)));
+float Weapon::hit() {
+    srand(static_cast<unsigned> (time(0)));
+    return minDamage + static_cast<float> (rand()) / (static_cast<float> (RAND_MAX / (maxDamage - minDamage)));
 }
 
-int main(int argc, char **argv) {
+void fight() {
     string bruteName = "Brute";
     Character<float> brute = Character<float>(bruteName, 100.0);
     Weapon oblivionMace = Weapon("Oblivion mace", 13, 21);
-    cout << "Brute: " << brute.getName() << ", hp: " << brute.getHitPoints()  << endl;
-    brute.dealDamage(oblivionMace);
-    cout << "Brute receives hit on DamageInterceptor: " << brute.getName() << ", hp: " << brute.getHitPoints()  << endl;
+    brute.setWeapon(oblivionMace);
+
+    string opponentName = "Arrger";
+    Character<float> arrgor = Character<float>(opponentName, 100.0);
+    Weapon talonsLance = Weapon("Talon's lance", 15, 19);
+    arrgor.setWeapon(talonsLance);
     CriticalDamageInterceptor* criticalDamageInterceptor = new CriticalDamageInterceptor();
-    brute.setDamageInterceptor(criticalDamageInterceptor);
-    brute.dealDamage(oblivionMace);
-    cout << "Brute receives hit on CriticalDamageInterceptor: " << brute.getName() << ", hp: " << brute.getHitPoints()  << endl;
+    arrgor.setDamageInterceptor(criticalDamageInterceptor);
+
+    do {
+        arrgor.receiveDamage(brute);
+        brute.receiveDamage(arrgor);
+        cout << "Brute, hp: " << brute.getHitPoints() << endl;
+        cout << "Arrgor, hp: " << arrgor.getHitPoints() << endl;
+    } while (brute.getHitPoints() > 0 && arrgor.getHitPoints() > 0);
+}
+
+int main(int argc, char **argv) {
+    fight();
+
     return 0;
 }
