b1f5546dfb286493ccc783a3e9af28faadff610e
Roman Subik <roman.subik@ocado.com>
2017-11-07 07:19:30 +0100

    Add GoogleTest framework.

---
+++ LearningPathBeginningCpp/30_handson_functions.hpp
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   30_handson_functions.hpp
+ * Author: roman.subik
+ *
+ * Created on 2 listopada 2017, 06:58
+ */
+
+#ifndef HANDSON_FUNCTIONS_HPP
+#define HANDSON_FUNCTIONS_HPP
+
+
+
+#endif /* HANDSON_FUNCTIONS_HPP */
+
--- LearningPathBeginningCpp/nbproject/Makefile-Debug.mk
+++ LearningPathBeginningCpp/nbproject/Makefile-Debug.mk
@@ -14,15 +14,15 @@ GREP=grep
 NM=nm
 CCADMIN=CCadmin
 RANLIB=ranlib
-CC=gcc
+CC=g++
 CCC=g++
 CXX=g++
 FC=gfortran
 AS=as
 
 # Macros
-CND_PLATFORM=GNU-Linux
-CND_DLIB_EXT=so
+CND_PLATFORM=GNU-MacOSX
+CND_DLIB_EXT=dylib
 CND_CONF=Debug
 CND_DISTDIR=dist
 CND_BUILDDIR=build
@@ -36,8 +36,21 @@ OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 # Object Files
 OBJECTFILES= \
 	${OBJECTDIR}/11_enhanced_helloworld.o \
-	${OBJECTDIR}/19_calc.o
+	${OBJECTDIR}/19_calc.o \
+	${OBJECTDIR}/30_handson_functions.o \
+	${OBJECTDIR}/brute.o \
+	${OBJECTDIR}/one_day_game.o
 
+# Test Directory
+TESTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tests
+
+# Test Files
+TESTFILES= \
+	${TESTDIR}/TestFiles/f1
+
+# Test Object Files
+TESTOBJECTFILES= \
+	${TESTDIR}/test.o
 
 # C Compiler Flags
 CFLAGS=
@@ -63,23 +76,127 @@ ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/learningpathbeginningcpp: ${OBJECTFIL
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
 	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/learningpathbeginningcpp ${OBJECTFILES} ${LDLIBSOPTIONS}
 
-${OBJECTDIR}/11_enhanced_helloworld.o: 11_enhanced_helloworld.cpp 
+${OBJECTDIR}/11_enhanced_helloworld.o: 11_enhanced_helloworld.cpp
 	${MKDIR} -p ${OBJECTDIR}
 	${RM} "$@.d"
 	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/11_enhanced_helloworld.o 11_enhanced_helloworld.cpp
 
-${OBJECTDIR}/19_calc.o: 19_calc.cpp 
+${OBJECTDIR}/19_calc.o: 19_calc.cpp
 	${MKDIR} -p ${OBJECTDIR}
 	${RM} "$@.d"
 	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/19_calc.o 19_calc.cpp
 
+${OBJECTDIR}/30_handson_functions.o: 30_handson_functions.cpp
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/30_handson_functions.o 30_handson_functions.cpp
+
+${OBJECTDIR}/brute.o: brute.cpp
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/brute.o brute.cpp
+
+${OBJECTDIR}/one_day_game.o: one_day_game.cpp
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/one_day_game.o one_day_game.cpp
+
 # Subprojects
 .build-subprojects:
+
+# Build Test Targets
+.build-tests-conf: .build-tests-subprojects .build-conf ${TESTFILES}
+.build-tests-subprojects:
+	cd ../../../NetBeansProjects/GoogleTest && ${MAKE}  -f Makefile CONF=Debug
+
+${TESTDIR}/TestFiles/f1: ${TESTDIR}/test.o ${OBJECTFILES:%.o=%_nomain.o}
+	${MKDIR} -p ${TESTDIR}/TestFiles
+	${LINK.cc} -o ${TESTDIR}/TestFiles/f1 $^ ${LDLIBSOPTIONS}   ../../../NetBeansProjects/GoogleTest/dist/Debug/GNU-MacOSX/libgoogletest.a 
+
+
+${TESTDIR}/test.o: test.cpp 
+	${MKDIR} -p ${TESTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I/Users/roman.subik/pworkspace/googletest/googletest -I/Users/roman.subik/pworkspace/googletest/googletest/include -std=c++11 -MMD -MP -MF "$@.d" -o ${TESTDIR}/test.o test.cpp
+
+
+${OBJECTDIR}/11_enhanced_helloworld_nomain.o: ${OBJECTDIR}/11_enhanced_helloworld.o 11_enhanced_helloworld.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/11_enhanced_helloworld.o`; \
+	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
+	then  \
+	    ${RM} "$@.d";\
+	    $(COMPILE.cc) -g -std=c++11 -Dmain=__nomain -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/11_enhanced_helloworld_nomain.o 11_enhanced_helloworld.cpp;\
+	else  \
+	    ${CP} ${OBJECTDIR}/11_enhanced_helloworld.o ${OBJECTDIR}/11_enhanced_helloworld_nomain.o;\
+	fi
+
+${OBJECTDIR}/19_calc_nomain.o: ${OBJECTDIR}/19_calc.o 19_calc.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/19_calc.o`; \
+	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
+	then  \
+	    ${RM} "$@.d";\
+	    $(COMPILE.cc) -g -std=c++11 -Dmain=__nomain -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/19_calc_nomain.o 19_calc.cpp;\
+	else  \
+	    ${CP} ${OBJECTDIR}/19_calc.o ${OBJECTDIR}/19_calc_nomain.o;\
+	fi
+
+${OBJECTDIR}/30_handson_functions_nomain.o: ${OBJECTDIR}/30_handson_functions.o 30_handson_functions.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/30_handson_functions.o`; \
+	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
+	then  \
+	    ${RM} "$@.d";\
+	    $(COMPILE.cc) -g -std=c++11 -Dmain=__nomain -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/30_handson_functions_nomain.o 30_handson_functions.cpp;\
+	else  \
+	    ${CP} ${OBJECTDIR}/30_handson_functions.o ${OBJECTDIR}/30_handson_functions_nomain.o;\
+	fi
+
+${OBJECTDIR}/brute_nomain.o: ${OBJECTDIR}/brute.o brute.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/brute.o`; \
+	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
+	then  \
+	    ${RM} "$@.d";\
+	    $(COMPILE.cc) -g -std=c++11 -Dmain=__nomain -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/brute_nomain.o brute.cpp;\
+	else  \
+	    ${CP} ${OBJECTDIR}/brute.o ${OBJECTDIR}/brute_nomain.o;\
+	fi
+
+${OBJECTDIR}/one_day_game_nomain.o: ${OBJECTDIR}/one_day_game.o one_day_game.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	@NMOUTPUT=`${NM} ${OBJECTDIR}/one_day_game.o`; \
+	if (echo "$$NMOUTPUT" | ${GREP} '|main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T main$$') || \
+	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
+	then  \
+	    ${RM} "$@.d";\
+	    $(COMPILE.cc) -g -std=c++11 -Dmain=__nomain -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/one_day_game_nomain.o one_day_game.cpp;\
+	else  \
+	    ${CP} ${OBJECTDIR}/one_day_game.o ${OBJECTDIR}/one_day_game_nomain.o;\
+	fi
+
+# Run Test Targets
+.test-conf:
+	@if [ "${TEST}" = "" ]; \
+	then  \
+	    ${TESTDIR}/TestFiles/f1 || true; \
+	else  \
+	    ./${TEST} || true; \
+	fi
 
 # Clean Targets
 .clean-conf: ${CLEAN_SUBPROJECTS}
 	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
-	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/learningpathbeginningcpp
 
 # Subprojects
 .clean-subprojects:
