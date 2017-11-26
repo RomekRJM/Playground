ca009ab889a5f3a53c1f76ace39fa7d06a1efe9a
Roman Subik <roman.subik@ocado.com>
2017-11-26 17:45:05 +0100

    Lesson 49: Generic programming part1

--- LearningPathBeginningCpp/37_handson_pointers.cpp
+++ LearningPathBeginningCpp/37_handson_pointers.cpp
@@ -9,7 +9,7 @@ void generate_frequency_data(const std::string& file_name) {
     display();
 }
 
-int main(int argc, char* argv[]) {
+int main37(int argc, char* argv[]) {
     std::string file_name = "2013_USDA_Farmers_Markets_Geographic_Data.csv";
     generate_frequency_data(file_name);
 }
