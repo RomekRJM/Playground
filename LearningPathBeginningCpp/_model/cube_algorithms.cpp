7c5f41f96e42cb738c85b98fd02a539d59c6a6f3
Roman Subik <roman.subik@ocado.com>
2018-02-22 16:23:24 +0100

    Set up cube algorithm lookup maps.

--- LearningPathBeginningCpp/_model/cube_algorithms.cpp
+++ LearningPathBeginningCpp/_model/cube_algorithms.cpp
@@ -36,5 +36,27 @@ const string CubeAlgorithm::FLIP_Z_CLOCKWISE_90 = "z";
 const string CubeAlgorithm::FLIP_Z_COUNTER_CLOCKWISE_90 = "z'";
 const string CubeAlgorithm::FLIP_UPSIDE_DOWN = "z2";
 
+void CubeAlgorithm::initRotations() {
+    rotations.insert(pair<string, Rotation>(ROTATE_FRONT_CLOCKWISE, FRONT_CLOCKWISE));
+    rotations.insert(pair<string, Rotation>(ROTATE_UP_CLOCKWISE, UP_CLOCKWISE));
+    rotations.insert(pair<string, Rotation>(ROTATE_BACK_CLOCKWISE, BACK_CLOCKWISE));
+    rotations.insert(pair<string, Rotation>(ROTATE_DOWN_CLOCKWISE, DOWN_CLOCKWISE));
+    rotations.insert(pair<string, Rotation>(ROTATE_RIGHT_CLOCKWISE, RIGHT_CLOCKWISE));
+    rotations.insert(pair<string, Rotation>(ROTATE_LEFT_CLOCKWISE, LEFT_CLOCKWISE));
+    rotations.insert(pair<string, Rotation>(ROTATE_FRONT_COUNTER_CLOCKWISE, FRONT_COUNTER_CLOCKWISE));
+    rotations.insert(pair<string, Rotation>(ROTATE_UP_COUNTER_CLOCKWISE, UP_COUNTER_CLOCKWISE));
+    rotations.insert(pair<string, Rotation>(ROTATE_BACK_COUNTER_CLOCKWISE, BACK_COUNTER_CLOCKWISE));
+    rotations.insert(pair<string, Rotation>(ROTATE_DOWN_COUNTER_CLOCKWISE, DOWN_COUNTER_CLOCKWISE));
+    rotations.insert(pair<string, Rotation>(ROTATE_RIGHT_COUNTER_CLOCKWISE, RIGHT_COUNTER_CLOCKWISE));
+    rotations.insert(pair<string, Rotation>(ROTATE_LEFT_COUNTER_CLOCKWISE, LEFT_COUNTER_CLOCKWISE));
+}
 
-
+void CubeAlgorithm::initSteps() {
+    flips.insert(pair<string, Flip>(FLIP_Y_CLOCKWISE_90, Y_CLOCKWISE_90));
+    flips.insert(pair<string, Flip>(FLIP_Y_COUNTER_CLOCKWISE_90, Y_COUNTER_CLOCKWISE_90));
+    flips.insert(pair<string, Flip>(FLIP_X_CLOCKWISE_90, X_CLOCKWISE_90));
+    flips.insert(pair<string, Flip>(FLIP_X_COUNTER_CLOCKWISE_90, X_COUNTER_CLOCKWISE_90));
+    flips.insert(pair<string, Flip>(FLIP_Z_COUNTER_CLOCKWISE_90, Z_COUNTER_CLOCKWISE_90));
+    flips.insert(pair<string, Flip>(FLIP_Z_COUNTER_CLOCKWISE_90, Z_COUNTER_CLOCKWISE_90));
+    flips.insert(pair<string, Flip>(FLIP_UPSIDE_DOWN, UPSIDE_DOWN));
+}
