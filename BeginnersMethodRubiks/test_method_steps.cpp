33f6454fb1bceff830f6dc81df11d1736e3c89d1
Roman Subik <roman.subik@ocado.com>
2018-03-15 06:05:59 +0100

    Add missing tests for method_steps.

--- BeginnersMethodRubiks/test_method_steps.cpp
+++ BeginnersMethodRubiks/test_method_steps.cpp
@@ -38,9 +38,128 @@ BOOST_AUTO_TEST_CASE(test_is_white_cross_done_incomplete_cross) {
     BOOST_CHECK(!BeginnersMethod().isWhiteCrossDone(cube));
 }
 
-BOOST_AUTO_TEST_CASE(test_is_white_cross_done_incomplete_sides) {
+BOOST_AUTO_TEST_CASE(test_are_first_layer_corners_done) {
     Cube cube = CubeGenerator::fromString(
-            "GRRBBGBRG,YWWWYWYWW,ROGGGBOOR,WYWYWYYYY,BGGORROGB,OBROORBBO"
+            "YRGROOOOO,RYYYYYOYY,GOYORRRRR,WWWWWWWWW,OGRBBGBBB,BBBBGGGGG"
             );
-    BOOST_CHECK(!BeginnersMethod().isWhiteCrossDone(cube));
+    BOOST_CHECK(BeginnersMethod().areFirstLayerCornersDone(cube));
+}
+
+BOOST_AUTO_TEST_CASE(test_are_first_layer_corners_done_misplaced_corner) {
+    Cube cube = CubeGenerator::fromString(
+            "YRWROYOOG,YYGYYOOYO,YGRORRRRR,WWOWWWWWW,BBROBGYBB,GBBBGGGGG"
+            );
+    BOOST_CHECK(!BeginnersMethod().areFirstLayerCornersDone(cube));
+}
+
+BOOST_AUTO_TEST_CASE(test_are_first_layer_corners_done_rotated_cube) {
+    Cube cube = CubeGenerator::fromString(
+            "YGBOBBGBB,BRRBRRBOR,GBYGGRGGG,ROOGOOORO,WWWWWWWWW,OYRYYYYYY"
+            );
+    BOOST_CHECK(BeginnersMethod().areFirstLayerCornersDone(cube));
+}
+
+BOOST_AUTO_TEST_CASE(test_are_second_layer_edges_done) {
+    Cube cube = CubeGenerator::fromString(
+            "YOROOOOOO,OBYRYYOYY,RYYRRRRRR,WWWWWWWWW,GGBBBBBBB,GYBGGGGGG"
+            );
+    BOOST_CHECK(BeginnersMethod().areSecondLayerEdgesDone(cube));
+}
+
+BOOST_AUTO_TEST_CASE(test_are_second_layer_edges_done_misplaced_edge) {
+    Cube cube = CubeGenerator::fromString(
+            "YGOGGYGGG,YORBYYOYG,YGBBBBBBB,WWWWWWWWW,YRGOOOOOO,RYBRRRRRR"
+            );
+    BOOST_CHECK(!BeginnersMethod().areSecondLayerEdgesDone(cube));
+}
+
+BOOST_AUTO_TEST_CASE(test_is_yellow_dot_done) {
+    Cube cube = CubeGenerator::fromString(
+            "GYBGGGGGG,YYYBYYORO,GGBBBBBBB,WWWWWWWWW,YOROOOOOO,RYYRRRRRR"
+            );
+    BOOST_CHECK(BeginnersMethod().isYellowDotDone(cube));
+}
+
+BOOST_AUTO_TEST_CASE(test_is_yellow_dot_done_misplaced_dot) {
+    Cube cube = CubeGenerator::fromString(
+            "GYBGGGGGG,YYYBWYORO,GGBBBBBBB,WWWWWWWWW,YOROOOOOO,RYYRRRRRR"
+            );
+    BOOST_CHECK(!BeginnersMethod().isYellowDotDone(cube));
+}
+
+BOOST_AUTO_TEST_CASE(test_is_yellow_line_done) {
+    Cube cube = CubeGenerator::fromString(
+            "OYYRRRRRR,OBYYYYYOR,GYBOOOOOO,WWWWWWWWW,BGRGGGGGG,YRGBBBBBB"
+            );
+    BOOST_CHECK(BeginnersMethod().isYellowLineDone(cube));
+}
+
+BOOST_AUTO_TEST_CASE(test_is_yellow_line_done_mising_center) {
+    Cube cube = CubeGenerator::fromString(
+            "OYYRRRRRR,OBYYWYYOR,GYBOOOOOO,WWWWYWWWW,BGRGGGGGG,YRGBBBBBB"
+            );
+    BOOST_CHECK(!BeginnersMethod().isYellowLineDone(cube));
+}
+
+BOOST_AUTO_TEST_CASE(test_is_yellow_arc_done) {
+    Cube cube = CubeGenerator::fromString(
+            "GYBGGGGGG,YYYBYYORO,GGBBBBBBB,WWWWWWWWW,YOROOOOOO,RYYRRRRRR"
+            );
+    BOOST_CHECK(BeginnersMethod().isYellowArcDone(cube));
+}
+
+BOOST_AUTO_TEST_CASE(test_is_yellow_arc_done_mising_center) {
+    Cube cube = CubeGenerator::fromString(
+            "GYBGGGGGG,YYYBWYORO,GGBBBBBBB,WWWWWWWWW,YOROOOOOO,RYYRRRRRR"
+            );
+    BOOST_CHECK(!BeginnersMethod().isYellowArcDone(cube));
+}
+
+BOOST_AUTO_TEST_CASE(test_is_yellow_cross_done) {
+    Cube cube = CubeGenerator::fromString(
+            "YOOGGGGGG,RYYYYYGYY,GRYBBBBBB,WWWWWWWWW,BGROOOOOO,BBORRRRRR"
+            );
+    BOOST_CHECK(BeginnersMethod().isYellowCrossDone(cube));
+}
+
+BOOST_AUTO_TEST_CASE(test_is_yellow_cross_done_incompleted) {
+    Cube cube = CubeGenerator::fromString(
+            "GYBGGGGGG,YYYBYYORO,GGBBBBBBB,WWWWWWWWW,YOROOOOOO,RYYRRRRRR"
+            );
+    BOOST_CHECK(!BeginnersMethod().isYellowCrossDone(cube));
+}
+
+BOOST_AUTO_TEST_CASE(test_are_last_layer_corners_positioned) {
+    Cube cube = CubeGenerator::fromString(
+            "GRYGGGGGG,YYOYYYYYO,YOBBBBBBB,WWWWWWWWW,GBBOOOOOO,RGRRRRRRR"
+            );
+    BOOST_CHECK(BeginnersMethod().areLastLayerCornersPositioned(cube));
+}
+
+BOOST_AUTO_TEST_CASE(test_are_last_layer_corners_positioned_incompleted) {
+    Cube cube = CubeGenerator::fromString(
+            "BBOGGGGGG,YYYYYYRYG,BGRBBBBBB,WWWWWWWWW,YOOOOOOOO,GRYRRRRRR"
+            );
+    BOOST_CHECK(!BeginnersMethod().areLastLayerCornersPositioned(cube));
+}
+
+BOOST_AUTO_TEST_CASE(test_are_last_layer_corners_permuted) {
+    Cube cube = CubeGenerator::fromString(
+            "GGGGGGRGR,WWWWWWWWW,BBBBBBOBO,YYYYYYYYY,RRRRRRBOB,OOOOOOGRG"
+            );
+    BOOST_CHECK(BeginnersMethod().areLastLayerCornersPermuted(cube));
+}
+
+BOOST_AUTO_TEST_CASE(test_are_last_layer_corners_permuted_incompleted) {
+    Cube cube = CubeGenerator::fromString(
+            "GRYGGGGGG,YYOYYYYYO,YOBBBBBBB,WWWWWWWWW,GBBOOOOOO,RGRRRRRRR"
+            );
+    BOOST_CHECK(!BeginnersMethod().areLastLayerCornersPermuted(cube));
+}
+
+BOOST_AUTO_TEST_CASE(test_is_solved) {
+    Cube cube = CubeGenerator::fromString(
+            "GGGGGGGGG,YYYYYYYYY,BBBBBBBBB,WWWWWWWWW,OOOOOOOOO,RRRRRRRRR"
+            );
+    BOOST_CHECK(BeginnersMethod().isSolved(cube));
 }
