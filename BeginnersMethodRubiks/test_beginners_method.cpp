68277902dca0a2b658356608ee660bc250273a5b
Roman Subik <roman.subik@ocado.com>
2018-07-10 06:09:38 +0200

    Break long line.

--- BeginnersMethodRubiks/test_beginners_method.cpp
+++ BeginnersMethodRubiks/test_beginners_method.cpp
@@ -21,7 +21,16 @@ BOOST_AUTO_TEST_CASE(test_full_run) {
         // On solved ( GRW on FRU ): D'2 F'2 U' L'2 D'2 U R'2 D' F' L'2 D' U R' U'2 L' U L'2 B' L'2 F'2
         {
             "WGOYGORBW,WBRYWWBWG,YOGRBGRYY,BROBYROOW,YBGWRGGWB,RGOOORBYY",
-            "L,F,U,F,U,U,L,U,U,B',F,F,L,L,U,R,R,U,U,B,B,U,U,R,U,R',U',y,U,R,U,R',U',y,U,U,U,R,U,R',U',y,U,U,R,U,R',U',y,y,y,U,U,y,y,y,y,U',L',U',L,U,y',R,U,R',U',F,R,U,R',U',F',U,y,y,y,R,U,R',U',R,U,R',U',R,U,R',U',y,L',U',L,U,L',U',L,U,L',U',L,U,z2,R,U,R',U',R,U,R',U',R,U,R',U',R,U,R',U',D',R,U,R',U',R,U,R',U',D',R,U,R',U',R,U,R',U',R,U,R',U',R,U,R',U',D',R,U,R',U',R,U,R',U'z2,D',D',D',D',D',R,U,R',U',L',U',L,U,R,U,R',U',R,U,R',U',R,U,R',U',R,U,R',U',R,U,R',U',L',U',L,U,L',U',L,U,L',U',L,U,L',U',L,U,L',U',L,U,R,U,R',U',L',U',L,U,R,U,R',U',R,U,R',U',R,U,R',U',R,U,R',U',R,U,R',U',L',U',L,U,L',U',L,U,L',U',L,U,L',U',L,U,L',U',L,U"
+            "L,F,U,F,U,U,L,U,U,B',F,F,L,L,U,R,R,U,U,B,B,U,U,R,U,R',U',y,"
+            "U,R,U,R',U',y,U,U,U,R,U,R',U',y,U,U,R,U,R',U',y,y,y,U,U,y,y,y,y,"
+            "U',L',U',L,U,y',R,U,R',U',F,R,U,R',U',F',U,y,y,y,R,U,R',U',"
+            "R,U,R',U',R,U,R',U',y,L',U',L,U,L',U',L,U,L',U',L,U,z2,R,U,R',U',"
+            "R,U,R',U',R,U,R',U',R,U,R',U',D',R,U,R',U',R,U,R',U',D',R,U,R',U',"
+            "R,U,R',U',R,U,R',U',R,U,R',U',D',R,U,R',U',R,U,R',U'z2,D',D',D',"
+            "D',D',R,U,R',U',L',U',L,U,R,U,R',U',R,U,R',U',R,U,R',U',R,U,R',U',"
+            "R,U,R',U',L',U',L,U,L',U',L,U,L',U',L,U,L',U',L,U,L',U',L,U,"
+            "R,U,R',U',L',U',L,U,R,U,R',U',R,U,R',U',R,U,R',U',R,U,R',U',"
+            "R,U,R',U',L',U',L,U,L',U',L,U,L',U',L,U,L',U',L,U,L',U',L,U"
         }
     };
     
