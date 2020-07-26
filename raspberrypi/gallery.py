c2d8e8462b8fe00358f13ba8a1b1aeeba72628f3
Roman Subik <roman.subik@ocado.com>
2020-07-26 07:43:17 +0200

    Clone repos part 1.

--- raspberrypi/gallery.py
+++ raspberrypi/gallery.py
@@ -1,32 +1,42 @@
-import requests
 import os
+
+from git import Repo
+import requests
+
+
+GALLERY_PROJECTS={"query":  "{ group(fullPath: \"rsgallery\") { id name projects { nodes { name } } } }"}
+PATH="/tmp"
 
 class GitlabClient:
     def __init__(self, private_token):
-        self.url = "https://gitlab.com/api/v4/"
+        self.url = "https://gitlab.com/api/graphql"
         self.private_token = private_token
 
-    def __build_url(self, path, params):
-        base = "{}/{}?private_token={}".format(self.url, path, self.private_token)
 
-        for key, value in enumerate(params):
-            base += "&{}={}".format(key, value)
-
-        return base
-
-    def list_projects(self, search_criteria):
-        url = self.__build_url("projects", {
-            "search", "gal2",
-            "simple", "true",
-            "owned", "true"
-        })
-
-        response = requests.get(url=url)
+    def list_projects_in_group(self, group_name):
+        response = requests.post(
+            url=self.url,
+            params=GALLERY_PROJECTS,
+            headers={
+                "Authorization": "Bearer {}".format(self.private_token),
+                "Content-Type": "application/json"
+            }
+        )
         response.raise_for_status()
 
-        return response.json()
+        nodes = response.json()['data']['group']['projects']['nodes']
+
+        return [list(x.values())[0] for x in nodes]
+
+
+def clone_repo(repos):
+    for repo in repos:
+        Repo.clone_from("https://gitlab.com/{}".format(repo),
+                        "{}/{}".format(PATH, repo))
 
 
 if __name__ == "__main__":
+    GITLAB_GROUP = os.getenv("GITLAB_GROUP")
     GITLAB_TOKEN = os.getenv("GITLAB_TOKEN")
-    print(GitlabClient(GITLAB_TOKEN).list_projects(""))
+    projects = GitlabClient(GITLAB_TOKEN).list_projects_in_group(GITLAB_GROUP)
+    clone_repo(projects)
