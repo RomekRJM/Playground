af458fd66f26cc1c983e21b140a8968ea1981a21
Roman Subik <roman.subik@ocado.com>
2021-02-05 10:19:40 +0100

    Add info about bytes left in boot sector.

--- assembly/8086/bootloader/run.bash
+++ assembly/8086/bootloader/run.bash
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 nasm bootjeweled.asm  -f bin -o bootjeweled.bin
+
+TOTAL_BYTES=512
+ZEROES=`cat bootjeweled.bin | xxd -p | sed 's/0000//g' | wc -c`
+let "FREE_BYTES = $TOTAL_BYTES - (($ZEROES - 18) / 2)"
+echo "Bytes left: $FREE_BYTES out of $TOTAL_BYTES"
+
 qemu-system-i386 -fda bootjeweled.bin
