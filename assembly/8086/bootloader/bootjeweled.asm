9c2279f09e2e6652e12d0e39f0cf895281d5c2dc
Roman Subik <roman.subik@ocado.com>
2021-02-05 16:15:00 +0100

    Draw selection square

--- assembly/8086/bootloader/bootjeweled.asm
+++ assembly/8086/bootloader/bootjeweled.asm
@@ -7,38 +7,48 @@ int 10h     ; set it!
 
 game_loop:
 
+  draw_cursor:
+
+  mov al, 7
+  mov [col], word 10
+  mov [row], word 6
+  mov [col_e], word 68
+  mov [row_e], word 34
+  call draw_square
+
+  mov al, 8
   mov [col], word 14
   mov [row], word 10
   mov [col_e], word 34
   mov [row_e], word 30
 
-  inner_game_loop:
+  draw_gems:
 
-    call draw_square
-    mov ax, [col]
-    adc ax, 30
-    mov [col], ax
-    adc ax, 20
-    mov [col_e], ax
-    cmp ax, 320
-    jle inner_game_loop
+  call draw_square
+  mov ax, [col]
+  adc ax, 30
+  mov [col], ax
+  adc ax, 20
+  mov [col_e], ax
+  cmp ax, 320
+  jle draw_gems
 
-    mov [col], word 14
-    mov [col_e], word 34
+  mov [col], word 14
+  mov [col_e], word 34
 
-    mov ax, [row]
-    adc ax, 30
-    mov [row], ax
-    adc ax, 20
-    mov [row_e], ax
-    cmp ax, 200
-    jle inner_game_loop
+  mov ax, [row]
+  adc ax, 30
+  mov [row], ax
+  adc ax, 20
+  mov [row_e], ax
+  cmp ax, 200
+  jle draw_gems
+
 
   jmp game_loop
 
 
 draw_square:
-  call rand_color
   mov cx, [col]  ;col
   mov dx, [row]  ;row
 
@@ -58,8 +68,8 @@ draw_square:
   ret
 
 rand_color:
-  int 1ah    ; get number of ticks since midnight
-  mov al, dl
+  ;int 1ah    ; get number of ticks since midnight
+  mov al, 10
 
   ret
 
@@ -67,6 +77,11 @@ col: dw 14
 row: dw 10
 col_e: dw 34
 row_e: dw 30
+
+cur_y: dw 10
+cur_x: dw 8
+cur_ye: dw 38
+cur_xe: dw 60
 
 times 510-($-$$) db 0 ; fill the output file with zeroes until 510 bytes are full
 dw 0xaa55 ; magic number that tells the BIOS this is bootable
