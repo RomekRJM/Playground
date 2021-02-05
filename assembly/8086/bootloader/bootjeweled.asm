16e988a7c617d1bb185fc9b404805ef7587b3d4b
Roman Subik <roman.subik@ocado.com>
2021-02-05 11:07:50 +0100

    Drawing 10 x 6 red squares.

--- assembly/8086/bootloader/bootjeweled.asm
+++ assembly/8086/bootloader/bootjeweled.asm
@@ -7,25 +7,31 @@ int 10h     ; set it!
 
 game_loop:
 
+  mov [col], word 14
+  mov [row], word 10
+  mov [col_e], word 34
+  mov [row_e], word 30
+
   inner_game_loop:
 
     call draw_square
-    mov ax, col
+    mov ax, [col]
     adc ax, 30
-    mov [col_e], ax
-    adc ax, 20
     mov [col], ax
-    cmp ax, 290
+    adc ax, 20
+    mov [col_e], ax
+    cmp ax, 320
     jle inner_game_loop
 
-    mov ax, 10
-    mov [col], ax
+    mov [col], word 14
+    mov [col_e], word 34
+
     mov ax, [row]
     adc ax, 30
-    mov [row_e], ax
-    adc ax, 20
     mov [row], ax
-    cmp ax, 150
+    adc ax, 20
+    mov [row_e], ax
+    cmp ax, 200
     jle inner_game_loop
 
   jmp game_loop
@@ -35,26 +41,26 @@ draw_square:
   mov cx, [col]  ;col
   mov dx, [row]  ;row
 
-  colcount:
+  draw_line:
   inc cx
   mov ah, 0ch ; put pixel
   mov al, 0ch ; pick color
   int 10h
 
   cmp cx, [col_e]
-  jne colcount
+  jne draw_line
 
-  mov cx, [col]  ; reset to start of col
-  inc dx      ; next row
+  mov cx, [col]
+  inc dx
   cmp dx, [row_e]
-  jne colcount
+  jne draw_line
 
   ret
 
-col: dw 20
-row: dw 20
-col_e: dw 50
-row_e: dw 50
+col: dw 14
+row: dw 10
+col_e: dw 34
+row_e: dw 30
 
 times 510-($-$$) db 0 ; fill the output file with zeroes until 510 bytes are full
 dw 0xaa55 ; magic number that tells the BIOS this is bootable
