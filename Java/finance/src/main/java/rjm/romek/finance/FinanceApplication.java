4509314c0429948d22f17c40c45a412a2234ab59
Roman Subik <roman.subik@ocado.com>
2021-04-07 08:05:19 +0200

    Use CountDownLatch to prevent app from stopping.

--- Java/finance/src/main/java/rjm/romek/finance/FinanceApplication.java
+++ Java/finance/src/main/java/rjm/romek/finance/FinanceApplication.java
@@ -1,13 +1,27 @@
 package rjm.romek.finance;
 
+import java.util.concurrent.CountDownLatch;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@Slf4j
 public class FinanceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(FinanceApplication.class, args);
-    }
+  @Bean
+  public CountDownLatch closeLatch() {
+    return new CountDownLatch(1);
+  }
 
+  public static void main(String[] args) throws InterruptedException {
+    ConfigurableApplicationContext ctx = SpringApplication.run(FinanceApplication.class, args);
+    log.info("Use SIGKILL to stop the app.");
+
+    final CountDownLatch closeLatch = ctx.getBean(CountDownLatch.class);
+    Runtime.getRuntime().addShutdownHook(new Thread(() -> closeLatch.countDown()));
+    closeLatch.await();
+  }
 }
