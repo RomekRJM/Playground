091c71759eafc4f621bb34c1a392e6a6a1689d8d
Roman Subik <roman.subik@ocado.com>
2022-05-20 14:44:17 +0200

    Buying flow for Tauron.

---
+++ Python/coal/tauron.py
@@ -0,0 +1,41 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium_stealth import stealth
+from time import sleep
+
+chrome_options = Options()
+# chrome_options.add_argument("--disable-extensions")
+# chrome_options.add_argument("--disable-gpu")
+# chrome_options.add_argument("--headless")
+chrome_options.add_argument("--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36")
+chrome_options.add_argument('--disable-extensions')
+chrome_options.add_argument('--profile-directory=Default')
+chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+chrome_options.add_experimental_option('useAutomationExtension', False)
+# chrome_options.add_argument("--incognito")
+chrome_options.add_argument("--disable-plugins-discovery")
+chrome_options.add_argument("--enable-javascript")
+
+driver = webdriver.Chrome('/Users/roman.subik/Downloads/chromedriver', options=chrome_options)
+stealth(driver,
+        languages=["en-US", "en"],
+        vendor="Google Inc.",
+        platform="Win32",
+        webgl_vendor="Intel Inc.",
+        renderer="Intel Iris OpenGL Engine",
+        fix_hairline=True,
+        )
+
+start_url = "https://web.archive.org/web/20210513093544/https://sklep.tauron.pl/"
+driver.get(start_url)
+# print(driver.page_source.encode("utf-8"))
+driver.find_element(By.CLASS_NAME, 'wybieram').click()
+driver.execute_script(f"document.getElementById('postal-code-input').focus();")
+postal = driver.find_element(By.NAME, 'zip_code_id')
+postal.click()
+postal.send_keys("31542")
+
+# driver.quit()
