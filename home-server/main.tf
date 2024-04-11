7771fc135b35f36055cb1a2660b24674bfb8fd03
RomekRJM <romek.rjm@gmail.com>
2024-04-11 06:15:10 +0200

    Add nginx config.

--- home-server/main.tf
+++ home-server/main.tf
@@ -35,18 +35,27 @@ resource "docker_container" "minecraft" {
 }
 
 resource "docker_image" "nginx" {
-  
   name         = "nginx"
   keep_locally = false
 }
 
 resource "docker_container" "nginx" {
   image = docker_image.nginx.image_id
-  name  = "tutorial"
+  name  = "nginx"
+
+  upload {
+    file = "/etc/nginx/conf.d/eryk.nginx.conf"
+    source = "${path.cwd}/nginx.conf"
+  }
 
   ports {
     internal = 80
     external = 8000
+  }
+
+  volumes {
+    host_path = "${path.cwd}/eryk-sonic"
+    container_path = "/usr/share/nginx/eryk-sonic"
   }
 }
 
