ead0615c76893463ace80b5af36f671174e59e64
Roman Subik <roman.subik@ocado.com>
2024-04-15 09:31:46 +0200

    Add new mod.

--- home-server/main.tf
+++ home-server/main.tf
@@ -1,9 +1,10 @@
 locals {
   minecraft_default_envs = {
-    EULA = "TRUE"
-    WHITELIST = "ErykCoco,"
-    OPS = "ErykCoco,"
-    REMOVE_OLD_MODS = "true"
+    EULA                    = "TRUE"
+    WHITELIST               = "ErykCoco,"
+    OPS                     = "ErykCoco,"
+    REMOVE_OLD_MODS         = "true"
+    REMOVE_OLD_MODS_EXCLUDE = ""
   }
 
   minecraft_default_config = {
@@ -13,7 +14,28 @@ locals {
     }
   }
 
-  current_minecraft_config = local.minecraft_default_config
+  grinch_christmas_config = {
+    tag = "latest"
+    env = {
+      "TYPE"             = "SPIGOT",
+      "SPIGET_RESOURCES" = "108842",
+      "INIT_MEMORY"      = "2G",
+      "MAX_MEMORY"       = "3G",
+    }
+  }
+
+  castle_siege_config = {
+    tag = "java17-alpine"
+    env = {
+      "TYPE"             = "SPIGOT",
+      "SPIGET_RESOURCES" = "115123",
+      "INIT_MEMORY"      = "1G",
+      "MAX_MEMORY"       = "2G",
+      "VERSION"          = "1.20"
+    }
+  }
+
+  current_minecraft_config = local.castle_siege_config
 }
 
 resource "docker_image" "minecraft" {
@@ -22,21 +44,22 @@ resource "docker_image" "minecraft" {
 }
 
 resource "docker_container" "minecraft" {
-  image = docker_image.minecraft.image_id
-  name  = "minecraft-server"
+  image        = docker_image.minecraft.image_id
+  name         = "minecraft-server"
   network_mode = "host"
-  restart = "always"
+  restart      = "unless-stopped"
+  stdin_open   = true
+  tty          = true
+
   env = toset(
     [
-      for key, value in merge(local.minecraft_default_envs, local.current_minecraft_config.env):
-        "${key}=${value}"
+      for key, value in merge(local.minecraft_default_envs, local.current_minecraft_config.env) :
+      "${key}=${value}"
     ]
   )
-  stdin_open = true
-  tty = true
 
   volumes {
-    host_path = "/tmp"
+    host_path      = "/tmp"
     container_path = "/data"
   }
 
@@ -52,13 +75,13 @@ resource "docker_image" "nginx" {
 }
 
 resource "docker_container" "nginx" {
-  image = docker_image.nginx.image_id
-  name  = "nginx"
+  image        = docker_image.nginx.image_id
+  name         = "nginx"
   network_mode = "host"
-  restart = "always"
+  restart      = "unless-stopped"
 
   upload {
-    file = "/etc/nginx/conf.d/eryk.nginx.conf"
+    file   = "/etc/nginx/conf.d/eryk.nginx.conf"
     source = "${path.cwd}/nginx.conf"
   }
 
@@ -68,7 +91,7 @@ resource "docker_container" "nginx" {
   }
 
   volumes {
-    host_path = "${path.cwd}/eryk-sonic"
+    host_path      = "${path.cwd}/eryk-sonic"
     container_path = "/usr/share/nginx/html/eryk-sonic"
   }
 }
